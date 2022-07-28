6c388638a069314eb13f64ce63c9bf238034d4ac
Vladimir <vist-amur@yandex.ru>
2022-07-28 04:14:04 +0300

    28072022

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ from kivymd.uix.boxlayout import MDBoxLayout
 from kivy.core.window import Window
 #from tkinter import *
 
-
 # root = Tk()
 # monitor_height = root.winfo_screenheight()
 # monitor_width = root.winfo_screenwidth()
-
 
 Window.maximize()
 #Window.size = (monitor_width , monitor_height)
